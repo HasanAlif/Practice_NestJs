e2322979dcf2fe83efa59cd6c386d2cc5357ec08
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-27 01:33:41 +0600

    get post example

---
+++ first-app/src/property/property.controller.ts
@@ -0,0 +1,14 @@
+import { All, Controller, Get, Post } from '@nestjs/common';
+
+@Controller('property')
+export class PropertyController {
+    @Get()
+    findAll() {
+        return "All properties";
+    }
+
+    @Post()
+    create() {
+        return "Property created";
+    }
+}
