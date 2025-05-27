f0a3498fa12575717e129232f517a9a4eb82010e
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-27 12:03:16 +0600

    Validation pipe for request parameter

--- first-app/src/property/property.controller.ts
+++ first-app/src/property/property.controller.ts
@@ -1,4 +1,4 @@
-import { All, Controller, Get, Post } from '@nestjs/common';
+import {  Body, Controller, Get, Param, ParseBoolPipe, ParseIntPipe, Post, Query } from '@nestjs/common';
 
 @Controller('property')
 export class PropertyController {
@@ -7,8 +7,19 @@ export class PropertyController {
         return "All properties";
     }
 
+    @Get(':id')
+    findOne(@Param('id',ParseIntPipe) id, @Query('sort', ParseBoolPipe) sort) {
+
+        console.log(typeof id); // number
+        console.log(typeof sort); // boolean
+
+        return `Property with ID: ${id}`;
+
+    }
+
     @Post()
-    create() {
+    create(@Body() body) {
+        console.log('Creating property with data:', body);
         return "Property created";
     }
 }
