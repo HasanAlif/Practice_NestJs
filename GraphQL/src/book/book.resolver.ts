e2b55de425578bc138d035b3bc72748ace43d5b8
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-29 12:43:51 +0600

    GraphQL Code First

--- GraphQL/src/book/book.resolver.ts
+++ GraphQL/src/book/book.resolver.ts
@@ -1,16 +1,20 @@
 import { Query, Resolver } from "@nestjs/graphql";
+import { Book } from "./book.schema";
+import { Book as BookModel } from "../graphql";
 
-@Resolver()
+@Resolver(of => Book)
 export class BookResolver {
 
-    @Query("books")
+    @Query(returns => [Book], { name: 'books' })
     getAllBooks() {
-        return[
+        let arr : BookModel[] = [
             {id:1, title: "Book One", price:455},
             {id:2, title: "Book Two", price:480},
             {id:3, title: "Book Three", price:550},
             {id:4, title: "Book Four", price:380},
             {id:5, title: "Book Five", price:400},
         ];
+
+        return arr;
     }
 }
