cfa5b5203b10c5f45b996f861914205fe9ca4d00
HasanAlif <alifalmehedihasan@gmail.com>
2025-07-27 22:26:51 +0600

    GraphQL Schema & Resolver

--- PgSQL/src/book/book.module.ts
+++ PgSQL/src/book/book.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { BookEntity } from './entity/book.entity';
 import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { BookService } from './book.service';
+import { BookResolver } from './book.resolver';
 
 
 @Module({
   imports: [TypeOrmModule.forFeature([BookEntity])],
   controllers: [],
-  providers: [BookService],
+  providers: [BookService, BookResolver],
 })
 export class BookModule {}
