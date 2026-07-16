3c96130c43294ee560fc4ccb29a0675411cb4c26
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2026-07-16 16:42:40 +0600

    Add Welcome_Back_With_Auth project

---
+++ Welcome_Back_With_Auth/src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  login() {
+    return { message: 'Login success' };
+  }
+  register() {
+    return { message: 'Register success' };
+  }
+}
