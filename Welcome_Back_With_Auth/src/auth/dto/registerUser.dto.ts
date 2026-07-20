aec287cd8751da45ff2fab62608f1b48b5ca547b
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2026-07-20 15:18:56 +0600

    Use Validation Pipe

--- Welcome_Back_With_Auth/src/auth/dto/registerUser.dto.ts
+++ Welcome_Back_With_Auth/src/auth/dto/registerUser.dto.ts
@@ -1,6 +1,19 @@
+import { IsNotEmpty, IsString, IsEmail, MinLength } from 'class-validator';
+
 export class RegisterDto {
+  @IsNotEmpty()
+  @IsString()
   firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
   lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
   password: string;
 }
--- Welcome_Back_With_Auth/src/main.ts
+++ Welcome_Back_With_Auth/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dns from 'dns';
+import { ValidationPipe } from '@nestjs/common';
 
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
