8775e25f8d2b2e1d090c697a1872e528d1b6b740
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2026-07-20 16:15:22 +0600

    AuthGuard With Get Profile

--- Welcome_Back_With_Auth/src/auth/auth.controller.ts
+++ Welcome_Back_With_Auth/src/auth/auth.controller.ts
@@ -1,11 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/registerUser.dto';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from './auth.guard';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @Post('register')
   async register(@Body() registerUserDto: RegisterDto) {
@@ -16,5 +28,18 @@ export class AuthController {
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  async getProfile(@Request() req: any) {
+    const userId = req.user.sub;
+    const user = await this.userService.getUserById(userId);
+    return {
+      id: user?._id,
+      firstName: user?.firstName,
+      lastName: user?.lastName,
+      email: user?.email,
+    };
   }
 }
--- Welcome_Back_With_Auth/src/user/user.service.ts
+++ Welcome_Back_With_Auth/src/user/user.service.ts
@@ -29,6 +29,10 @@ export class UserService {
   }
 
   async findByEmail(email: string) {
-    return this.userModel.findOne({ email }).exec();
+    return await this.userModel.findOne({ email }).exec();
+  }
+
+  async getUserById(id: string) {
+    return await this.userModel.findOne({ _id: id }).exec();
   }
 }
